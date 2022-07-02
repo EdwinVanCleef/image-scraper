77c93507e28ecaf4456d89fd57dc998c65406ccc
Joel Carlo Menor <menorjc@live.com>
2022-07-02 16:51:38 +0800

    Initial Commit

---
+++ WebScraper/Controllers/ImageParserController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebScraper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageParserController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        public IActionResult GetImageCount()
+        {
+            string url = "https://onfido.com/";
+            var response = Parser.CallUrl(url).Result;
+            return Ok(Parser.ImageCount(response));
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string url = "https://onfido.com/";
+            var response = Parser.CallUrl(url).Result;
+            var imgList = Parser.ParseHTML(response);
+            return Ok(imgList);
+        }
+    }
+}
---
+++ WebScraper/Controllers/Parser.cs
@@ -0,0 +1,56 @@
+﻿using HtmlAgilityPack;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using System;
+using WebScraper.Models;
+
+namespace WebScraper.Controllers
+{
+    public class Parser
+    {
+        public static async Task<string> CallUrl(string fullUrl)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync(fullUrl);
+            return response;
+        }
+
+        public static int ImageCount(string html)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var images = htmlDoc.DocumentNode.SelectNodes("//img");
+            return images.Count;
+        }
+
+        public static List<ImageInfo> ParseHTML(string html)
+        {
+            HtmlDocument htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
+
+            var images = htmlDoc.DocumentNode.SelectNodes("//img");
+            List<ImageInfo> imageList = new List<ImageInfo>();
+
+            foreach (var item in images)
+            {
+                var imageInfo = new ImageInfo
+                {
+                    Url = item.OuterHtml.ToString(),
+                    Size = 0
+                };
+
+                imageList.Add(imageInfo);
+            }
+
+            return imageList;
+        }
+
+        
+    }
+}
